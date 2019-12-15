3f62ded89144bdbd2136385ec69a8aa284d574c0
System Administrator <root@Ambers-MacBook-Pro.local>
2019-12-14 18:54:33 -0600

    Updates

---
+++ precache-manifest.27cff07f484040501db2e3de4e6a7472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224a995cd0b4793735a2eea15fac1dc1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "059a92faa6b46d77725f",
+    "url": "/robofriends/static/css/main.cc9db9a9.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "059a92faa6b46d77725f",
+    "url": "/robofriends/static/js/main.740ac6e9.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
